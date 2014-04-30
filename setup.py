6ab4ec12a43c41e28e5360e44bc944a84b3993fe
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-04-30 13:40:09 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -11,9 +11,17 @@ setup(name='eight',
       description='8-Puzzle Solver',
       author='Yusuke Miyazaki',
       author_email='miyazaki.dev@gmail.com',
+      url='https://github.com/litesystems/8puzzle',
       packages=['eight'],
       scripts=['scripts/eightpuzzle'],
       test_suite='tests',
+      install_requires=[
+          'matplotlib>=1.3.1',
+          'networkx>=1.8.1'
+      ],
       classifiers=[
-          'Development Status :: 3 - Alpha'
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
       ])
